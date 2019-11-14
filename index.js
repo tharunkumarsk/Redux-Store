ef8ae81f25d648acfca2ced45481cfb2996679a1
Tharunkumar <tharunkumarsk@gmail.com>
2019-11-14 18:53:27 +0100

    Add subscribe in store to access the methods in it

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 function createStore(){
 
     let state;
+    let listners =[]
 
     const getState = () => state;
+    const subscribe = (listner) =>{
+        listners.push(listner);
+        return () => {
+            listeners = listeners.filter((l) => l !== listener)
+          }
+    }
 
     return{
-        getState
+        getState,
+        subscribe
     }
 
 
