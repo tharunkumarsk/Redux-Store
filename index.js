8fe81cd41c140eafac3c518aec7da256c32ef3ea
Tharunkumar <tharunkumarsk@gmail.com>
2019-11-15 13:08:20 +0100

    Add equalent javascrit function for subscribe for ref

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-function createStore(){
+function createStore() {
 
     let state;
-    let listners =[]
+    let listners = []
 
     const getState = () => state;
-    const subscribe = (listner) =>{
+    const subscribe = (listner) => {
         listners.push(listner);
         return () => {
             listeners = listeners.filter((l) => l !== listener)
-          }
+        }
     }
+    /**
+     * Samwe as above arrow function not to confuse 
+     */
 
-    return{
+    /*var subscribe = function subscribe(listener) {
+        listeners.push(listener);
+        return function () {
+            listeners = listeners.filter(function (l) {
+                return l !== listener;
+            });
+        };
+    };*/
+
+
+    return {
         getState,
         subscribe
     }
