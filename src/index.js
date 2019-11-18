f576e10dac10734531779a3abcdcbdd40a26e5e7
tharunkumarsk <tharunkumarsk@gmail.com>
2019-11-19 00:29:03 +0100

    Basic UI with all functions integrated

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+var $ = require("jquery")
 
 // Library Code
 
@@ -39,150 +39,137 @@ function createStore(reducer) {
 
 
 }
+function generateId () {
+    return Math.random().toString(36).substring(2);
+  }
 
 
 //app code
-const constants = 
-    { 
-        ADD_TODO :'ADD_TODO',
-        REMOVE_TODO:'REMOVE_TODO',
-        TOGGLE_TODO:"TOGGLE_TODO",
-        ADD_GOAL:"ADD_GOAL",
-        REMOVE_GOAL:"REMOVE_GOAL"
-    };
-
-function addTodoAction(todo){
-return {
-    type:constants.ADD_TODO,
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+function addTodoAction (todo) {
+  return {
+    type: ADD_TODO,
     todo,
+  }
 }
-}
-
-function removeTodoAction(id){
-return {
-    type:constants.REMOVE_TODO,
+function removeTodoAction (id) {
+  return {
+    type: REMOVE_TODO,
     id,
+  }
 }
-}
-
-function toggleTodoAction(id){
-return {
-    type:constants.TOGGLE_TODO,
+function toggleTodoAction (id) {
+  return {
+    type: TOGGLE_TODO,
     id,
+  }
 }
+function addGoalAction (goal) {
+  return {
+    type: ADD_GOAL,
+    goal,
+  }
 }
-function addGoalAction(goals){
-return {
-    type:constants.ADD_GOAL,
-    goals,
+function removeGoalAction (id) {
+  return {
+    type: REMOVE_GOAL,
+    id,
+  }
 }
+function todos (state = [], action) {
+  switch(action.type) {
+    case ADD_TODO :
+      return state.concat([action.todo])
+    case REMOVE_TODO :
+      return state.filter((todo) => todo.id !== action.id)
+    case TOGGLE_TODO :
+      return state.map((todo) => todo.id !== action.id ? todo :
+        Object.assign({}, todo, { complete: !todo.complete }))
+    default :
+      return state
+  }
 }
-
-function removeGoalAction(id){
-    return {
-        type:constants.REMOVE_GOAL,
-        id,
-    }
-    }
-
-function todos(state=[],action){
-    switch(action.type){
-        case constants.ADD_TODO:
-        return state.concat([action.todo])
-        
-        case constants.REMOVE_TODO:
-        return state.filter((todo) => todo.id !== action.id)
-        
-        case constants.TOGGLE_TODO:
-        return state.map((todo) => todo.id !== action.id ? todo:
-        Object.assign({},todo,{completed : !action.completed})
-        )
-        default :
-        return state
-
-    }
-    
+function goals (state = [], action) {
+  switch(action.type) {
+    case ADD_GOAL :
+      return state.concat([action.goal])
+    case REMOVE_GOAL :
+      return state.filter((goal) => goal.id !== action.id)
+    default :
+      return state
+  }
 }
-function goals(state=[],action){
-    switch(action.type){
-        case constants.ADD_GOAL:
-        return state.concat([action.goals])
-        
-        case constants.REMOVE_GOAL:
-        return state.filter((goal) => goal.id !== action.id)
-
-        default :
-        return state
-
-    }
-    
+function app (state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action),
+  }
 }
-
-function app (state = {},action){
-    return {
-        todos:todos(state.todos,action),
-        goals:goals(state.goals,action)
-    }
-}   
-//const store = createStore(goals)
-
-/*Output if you pass goal as reducer 
-New state value [ 'this week full angular' ]
-New state value [ 'this week full angular' ]
-New state value [ 'this week full angular' ]
-New state value [ 'this week full angular' ]
-New state value [ 'this week full angular' ]*/
-
-//const store = createStore(todos)
-
-/** Output if you pass todos as reducer 
- * New state value []
- *   New state value [ { id: 0, name: 'learn angular', completed: false } ]
- * New state value [ { id: 0, name: 'learn angular', completed: false },{ id: 1, name: 'hello world java', completed: false } ]
- * New state value [ { id: 1, name: 'hello world java', completed: false } ]
- * New state value [ { id: 1, name: 'hello world java', completed: true } ]
- */
 const store = createStore(app)
-
-/** Output if you pass app as reducer - means a root reducer whichis combination of all reducers in the app 
- * New state value { todos: [], goals: [ 'this week full angular' ] }
- * New state value { todos: [ { id: 0, name: 'learn angular', completed: false } ],goals: [ 'this week full angular' ] }
- * New state value { todos: [ { id: 0, name: 'learn angular', completed: false },{ id: 1, name: 'hello world java', completed: false } ],goals: [ 'this week full angular' ] }
- * New state value { todos: [ { id: 1, name: 'hello world java', completed: false } ],goals: [ 'this week full angular' ] }
- * New state value { todos: [ { id: 1, name: 'hello world java', completed: true } ],goals: [ 'this week full angular' ] }
- */
-
- 
 store.subscribe(() => {
-   console.log("New state value", store.getState())
+  const { goals, todos } = store.getState()
+  document.getElementById('goals').innerHTML = ''
+  document.getElementById('todos').innerHTML = ''
+  goals.forEach(addGoalToDOM)
+  todos.forEach(addTodoToDOM)
 })
-
-
-store.dispatch(addTodoAction({
-        id:0,
-        name:"learn angular",
-        completed:false
-
-}))
-
-store.dispatch(addTodoAction({
-        id:1,
-        name:"hello world java",
-        completed:false
-
-}))
-
-store.dispatch(removeTodoAction(0))
-
-store.dispatch(toggleTodoAction(1))
-
-store.dispatch(addGoalAction({
-    id:0,
-    goal:"this week full angular"
-}))
-
-store.dispatch(addGoalAction({
-    id:1,
-    goal:"next week full of react"
-}))
-store.dispatch(removeGoalAction(1))
+// DOM code
+function addTodo () {
+  const input = document.getElementById('todo')
+  const name = input.value
+  input.value = ''
+  store.dispatch(addTodoAction({
+    name,
+    complete: false,
+    id: generateId()
+  }))
+}
+function addGoal () {
+  const input = document.getElementById('goal')
+  const name = input.value
+  input.value = ''
+  store.dispatch(addGoalAction({
+    id: generateId(),
+    name,
+  }))
+}
+document.getElementById('todoBtn')
+  .addEventListener('click', addTodo)
+document.getElementById('goalBtn')
+  .addEventListener('click', addGoal)
+function createRemoveButton (onClick) {
+  const removeBtn = document.createElement('button')
+  removeBtn.innerHTML = 'X'
+  removeBtn.addEventListener('click', onClick)
+  return removeBtn
+}
+function addTodoToDOM (todo) {
+  const node = document.createElement('li')
+  const text = document.createTextNode(todo.name)
+  const removeBtn = createRemoveButton(() => {
+    store.dispatch(removeTodoAction(todo.id))
+  })
+  node.appendChild(text)
+  node.appendChild(removeBtn)
+  node.style.textDecoration = todo.complete ? 'line-through' : 'none'
+  node.addEventListener('click', () => {
+    store.dispatch(toggleTodoAction(todo.id))
+  })
+  document.getElementById('todos')
+    .appendChild(node)
+}
+function addGoalToDOM (goal) {
+  const node = document.createElement('li')
+  const text = document.createTextNode(goal.name)
+  const removeBtn = createRemoveButton(() => {
+    store.dispatch(removeGoalAction(goal.id))
+  })
+  node.appendChild(text)
+  node.appendChild(removeBtn)
+  document.getElementById('goals')
+    .append(node)
+}
