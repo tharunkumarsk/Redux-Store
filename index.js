d710ffb5f58623d11dbc3e6bac0e0a18787c24c3
Tharunkumar <tharunkumarsk@gmail.com>
2019-11-18 17:03:39 +0100

    Add methods for actions

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-const constants = 
-    { 
-        ADD_TODO :'ADD_TODO',
-        REMOVE_TODO:'REMOVE_TODO',
-        TOGGLE_TODO:"TOGGLE_TODO",
-        ADD_GOAL:"ADD_GOAL",
-        REMOVE_GOAL:"REMOVE_GOAL"
-    };
+
 
 // Library Code
 
@@ -49,17 +42,60 @@ function createStore(reducer) {
 
 
 //app code
+const constants = 
+    { 
+        ADD_TODO :'ADD_TODO',
+        REMOVE_TODO:'REMOVE_TODO',
+        TOGGLE_TODO:"TOGGLE_TODO",
+        ADD_GOAL:"ADD_GOAL",
+        REMOVE_GOAL:"REMOVE_GOAL"
+    };
+
+function addTodoAction(todo){
+return {
+    type:constants.ADD_TODO,
+    todo,
+}
+}
+
+function removeTodoAction(id){
+return {
+    type:constants.REMOVE_TODO,
+    id,
+}
+}
+
+function toggleTodoAction(id){
+return {
+    type:constants.TOGGLE_TODO,
+    id,
+}
+}
+function addGoalAction(goals){
+return {
+    type:constants.ADD_GOAL,
+    goals,
+}
+}
+
+function removeGoalAction(id){
+    return {
+        type:constants.REMOVE_GOAL,
+        id,
+    }
+    }
+
 function todos(state=[],action){
     switch(action.type){
         case constants.ADD_TODO:
         return state.concat([action.todo])
         
         case constants.REMOVE_TODO:
-        return state.filter((todo) => todo.id !== action.todo.id)
+        return state.filter((todo) => todo.id !== action.id)
         
         case constants.TOGGLE_TODO:
-        return state.map((todo) => todo.id !== action.todo.id ? todo:
-        Object.assign({},todo,{completed : !action.todo.completed})
+        return state.map((todo) => todo.id !== action.id ? todo:
+        Object.assign({},todo,{completed : !action.completed})
         )
         default :
         return state
@@ -70,7 +106,7 @@ function todos(state=[],action){
 function goals(state=[],action){
     switch(action.type){
         case constants.ADD_GOAL:
-        return state.concat([action.goal])
+        return state.concat([action.goals])
         
         case constants.REMOVE_GOAL:
         return state.filter((goal) => goal.id !== action.id)
@@ -121,41 +157,32 @@ store.subscribe(() => {
    console.log("New state value", store.getState())
 })
 
-store.dispatch({
-    type:constants.ADD_GOAL,
-    goal:"this week full angular"
-})
 
-store.dispatch({
-    type:constants.ADD_TODO,
-    todo:{
+store.dispatch(addTodoAction({
         id:0,
         name:"learn angular",
         completed:false
 
-    }
-})
-store.dispatch({
-    type:constants.ADD_TODO,
-    todo:{
+}))
+
+store.dispatch(addTodoAction({
         id:1,
         name:"hello world java",
         completed:false
 
-    }
-})
+}))
 
-store.dispatch({
-    type:constants.REMOVE_TODO,
-    todo:{
-        id:0
-    }
-})
+store.dispatch(removeTodoAction(0))
 
-store.dispatch({
-    type:constants.TOGGLE_TODO,
-    todo:{
-        id:1
+store.dispatch(toggleTodoAction(1))
 
-    }
-})
+store.dispatch(addGoalAction({
+    id:0,
+    goal:"this week full angular"
+}))
+
+store.dispatch(addGoalAction({
+    id:1,
+    goal:"next week full of react"
+}))
+store.dispatch(removeGoalAction(1))
