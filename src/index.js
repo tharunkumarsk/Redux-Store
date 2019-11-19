2cefe679a24fae5c9cecaa2a92cd6a73b587ac3a
Tharunkumar <tharunkumarsk@gmail.com>
2019-11-19 12:09:25 +0100

    Lesson challenge - add alert for goal and todo

--- src/index.js
+++ src/index.js
@@ -77,17 +77,18 @@ function goals(state = [], action) {
 }
 //Middleware functions
 const checker = (store) => (next) => (action) => {
-      if(
-        action.type === ADD_TODO &&
-        action.todo.name.toLowerCase().includes('bitcoin')
-      ){
+      if( action.type === ADD_TODO && action.todo.name.toLowerCase().includes('bitcoin') ){
         return alert('NO ! that is a bad Idear as of now ')
       }
-      if(
-        action.type === ADD_GOAL &&
-        action.goal.name.toLowerCase().includes('bitcoin')
-      ){
+      if( action.type === ADD_TODO){ 
+        alert(`Don't forget to ${action.todo.name}!`) 
+      }
+      if( action.type === ADD_GOAL && action.goal.name.toLowerCase().includes('bitcoin') ){
         return alert('NO ! that is a bad Idear as of now ')
+      }
+      if( action.type === ADD_GOAL){ 
+        alert(`That's a great
+        Goal!`)
       }
       return next(action)
     }
