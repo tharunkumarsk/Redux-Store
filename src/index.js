ae4e40a28d9ca90778aff003226fe55e08394086
Tharunkumar <tharunkumarsk@gmail.com>
2019-11-19 11:40:52 +0100

    Remove own checker and add redux middleware

--- src/index.js
+++ src/index.js
@@ -76,30 +76,28 @@ function goals(state = [], action) {
   }
 }
 
-function checkAndDispatch(store,action){
-  if(
-    action.type === ADD_TODO &&
-    action.todo.name.toLowerCase().includes('bitcoin')
-  ){
-    return alert('NO ! that is a bad Idear as of now ')
-  }
-  if(
-    action.type === ADD_GOAL &&
-    action.goal.name.toLowerCase().includes('bitcoin')
-  ){
-    return alert('NO ! that is a bad Idear as of now ')
-  }
-  return store.dispatch(action)
-
-
-
-}
+const checker = (store) => (next) => (action) => {
+      if(
+        action.type === ADD_TODO &&
+        action.todo.name.toLowerCase().includes('bitcoin')
+      ){
+        return alert('NO ! that is a bad Idear as of now ')
+      }
+      if(
+        action.type === ADD_GOAL &&
+        action.goal.name.toLowerCase().includes('bitcoin')
+      ){
+        return alert('NO ! that is a bad Idear as of now ')
+      }
+      return next(action)
+    }
+  
 // Create the store
 const store = Redux.createStore(
   Redux.combineReducers({
     todos,
     goals
-  }),
+  }),Redux.applyMiddleware(checker)
 );
 
 // subscribe to the listener
@@ -118,7 +116,7 @@ function addTodo(e) {
   e.preventDefault();
   const input = document.getElementById("todo");
   const name = input.value;
-  checkAndDispatch(store,
+  store.dispatch(
     addTodoAction({
       name,
       complete: false,
@@ -132,7 +130,7 @@ function addGoal(e) {
   e.preventDefault();
   const input = document.getElementById("goal");
   const name = input.value;
-  checkAndDispatch(store,
+  store.dispatch(
     addGoalAction({
       id: generateId(),
       name
